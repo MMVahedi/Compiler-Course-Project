58705b485bb4400266117e1f351341e5af599dc8
Mahdi Vahedi <m.m.vahedi13800@gmail.com>
2023-04-05 16:22:27 +0330

    Fill Scanner init

--- compiler.py
+++ compiler.py
@@ -3,11 +3,10 @@ Ali Razghandi 99109296
 Mohammad Mahdi Vahedi 99109314
 """
 
-import scanner
+from scanner import Scanner
 
-token = None
-while(True): #TODO token != EOF
-      
-      token = scanner.get_next_token()
-
-    
+scanner = Scanner('input.txt')
+# token = None
+# while(True): #TODO token != EOF
+#
+#       token = scanner.get_next_token()
--- scanner.py
+++ scanner.py
@@ -1,8 +1,28 @@
 class Scanner:
     def __init__(self, filename):
-        return None
+        self.filename = filename
+        with open(filename) as f:
+            self.lines = f.readlines()
+        self.lines = list(map(lambda x: x.replace('\n', ''), self.lines))
+        self.lineno = 1
+        self.line_pointer = 0
+        self.symbols = [';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '*', '=', '<']
+        self.whitespaces = ['\n', '\r', '\t', '\v', '\f', ' ']
 
-
-
-    def get_next_token():
-        return None
+    def get_next_token(self):
+        index = self.line_pointer
+        line = self.lines[self.lineno - 1]
+        current_token = ''
+        token_type = None
+        while True:
+            ch = line[index]
+            if ch in self.symbols:
+                pass
+            # elif is digit
+            # elif is letter
+            # elif == '/':
+            elif ch in self.whitespaces:
+                index += 1
+            else:
+                # TODO : Error handle con pedarsag
+                pass
