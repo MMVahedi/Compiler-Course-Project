8af42173ffd4f32221ed6e44c28d334a8f877933
Aquasar <a.razghandi11@gmail.com>
2023-04-05 15:50:07 +0330

    add get_next_token() to Scanner class

---
+++ compiler.py
@@ -0,0 +1,13 @@
+"""
+Ali Razghandi 99109296
+Mohammad Mahdi Vahedi 99109314
+"""
+
+import scanner
+
+token = None
+while(True): #TODO token != EOF
+      
+      token = scanner.get_next_token()
+
+    
---
+++ scanner.py
@@ -0,0 +1,8 @@
+class Scanner:
+    def __init__(self, filename):
+        return None
+
+
+
+    def get_next_token():
+        return None
