94ad3c29f19aa5e41774309e9fe412519ed1b5b2
Mahdi Vahedi <m.m.vahedi13800@gmail.com>
2023-05-10 17:14:12 +0330

    Add error message generator method.

--- Parser.py
+++ Parser.py
@@ -1,6 +1,16 @@
 from anytree import Node, RenderTree
 
 
+def generate_panic_mode_error_message(type, token, line_number):
+    # error type: {1,2,3, 4}. 1,2 and 3 are based on slides and 4 is for Unexpected EOF.
+    if type == 1:
+        return '#{} : syntax error, illegal {}'.format(line_number, token)
+    elif type == 4:
+        return '#{} : syntax error, Unexpected EOF'.format(line_number)
+    else:
+        return '#{} : syntax error, missing {}'.format(line_number, token)
+
+
 def generate_parse_tree_example():
     root = Node('root', parnet=None)
     a = Node('a', parent=root)
@@ -11,5 +21,6 @@ def generate_parse_tree_example():
     for pre, _, node in RenderTree(root):
         tree_str = u"%s%s" % (pre, node.name)
         print(tree_str.ljust(8))
+
 
 generate_parse_tree_example()
